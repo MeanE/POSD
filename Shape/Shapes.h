7d94a8ae19ba427ec51b696fab138fc693196edf
MingE <coopldh@gmail.com>
2016-09-24 00:17:13 +0800

    Complete computing the area of a triangle Complete computing the sum of perimeters of a number of shapes

--- Shape/Shapes.h
+++ Shape/Shapes.h
@@ -2,6 +2,8 @@
 #define SHAPES_H_INCLUDED
 
 #include <cmath>
+#include <vector>
+using namespace std;
 
 double PI=3.1415926;
 
@@ -15,8 +17,10 @@ class Circle : public Shape{
 public:
     Circle(double centerX, double centerY, double radius):
         cx(centerX),cy(centerY),r(radius){}
-    double area() const {return PI*r*r;}
-    double perimeter() const {return PI*2*r;}
+
+    double area() const{return PI*r*r;}
+    double perimeter() const{return PI*2*r;}
+
 private:
     double cx,cy,r;
 };
@@ -25,8 +29,10 @@ class Rectangle : public Shape{
 public:
     Rectangle(double leftTopX, double leftTopY, double length, double width):
         x(leftTopX),y(leftTopY),l(length),w(width){}
-    double area() const {return l*w;}
-    double perimeter() const {return 2*(l+w);}
+
+    double area() const{return l*w;}
+    double perimeter() const{return 2*(l+w);}
+
 private:
     double x,y,l,w;
 };
@@ -35,16 +41,36 @@ class Triangle : public Shape{
 public:
     Triangle(double x1, double y1, double x2, double y2, double x3, double y3):
         ax(x1),ay(y1),bx(x2),by(y2),cx(x3),cy(y3){}
-    double area() const {return 0;}
-    double perimeter() const {
+
+    /** Triangle
+      * Heron's formula
+      * use three point to compute area
+    ¡@¡@¡@¡@¡U¡@¡@¡@¡@¡@¡@¡@¡U
+¡@¡@¡@¡@¡@¢°¡UaX¡@bX¡@cX¡@aX¡U
+    area¡×¡Ð¡U¡@¡@¡@¡@¡@¡@¡@¡U
+¡@¡@¡@¡@¡@¢±¡UaY¡@bY¡@cY¡@aY¡U
+¡@¡@¡@*¡@¡@ ¡U¡@¡@¡@¡@¡@¡@¡@¡U*/
+    double area() const{
+        return abs((ax*by+ bx*cy+ cx*ay)-(ay*bx+ by*cx+ cy*ax))/2;
+    }
+
+    double perimeter() const{
         double abLength=sqrt(pow(ax-bx,2) + pow(ay-by,2));
         double acLength=sqrt(pow(ax-cx,2) + pow(ay-cy,2));
         double bcLength=sqrt(pow(bx-cx,2) + pow(by-cy,2));
 
-        return abLength + acLength + bcLength;
+        return abLength+ acLength+ bcLength;
     }
+
 private:
     double ax,ay,bx,by,cx,cy;
 };
 
+double sumOfPerimeter (vector<Shape*> shp){
+    double sum=0;
+    for(Shape* sp: shp){
+        sum+=sp->area();
+    }
+    return sum;
+}
 #endif // SHAPES_H_INCLUDED
--- Shape/UnitShapes.h
+++ Shape/UnitShapes.h
@@ -4,25 +4,38 @@
 #include "..\cppunitlite\TestHarness.h"
 #include "Shapes.h"
 
-const double EPSLION = 0.000001;
+const double EPSLION=0.000001;
 TEST (first, Circle) {
     Circle cir(0,0, 10);
-    DOUBLES_EQUAL(314.15926,cir.area(),EPSLION);
-    DOUBLES_EQUAL(62.831852,cir.perimeter(),EPSLION);
+
+    DOUBLES_EQUAL(314.15926, cir.area(), EPSLION);
+    DOUBLES_EQUAL(62.831852, cir.perimeter(), EPSLION);
     //FAIL("123456");
 }
 
 TEST (second, Rectangle) {
     Rectangle rect(0,0, 2,5);
-    DOUBLES_EQUAL(10,rect.area(),EPSLION);
-    DOUBLES_EQUAL(14,rect.perimeter(),EPSLION);
+
+    DOUBLES_EQUAL(10, rect.area(), EPSLION);
+    DOUBLES_EQUAL(14, rect.perimeter(), EPSLION);
     //FAIL("123456");
 }
 
 TEST (thrid, Triangle) {
     Triangle tri(1,-1, 4,-1, 4,3);
-    //DOUBLES_EQUAL(6,tri.area(),EPSLION);
-    DOUBLES_EQUAL(12,tri.perimeter(),EPSLION);
+
+    DOUBLES_EQUAL(6, tri.area(), EPSLION);
+    DOUBLES_EQUAL(12, tri.perimeter(), EPSLION);
+    //FAIL("123456");
+}
+
+TEST (fourth, sumOfPerimeter) {
+    Triangle tri(1,-1, 4,-1, 4,3);
+    Rectangle rect(0,0, 2,5);
+    Circle cir(0,0, 10);
+    vector<Shape*> shp{&tri, &rect, &cir};
+
+    DOUBLES_EQUAL(330.15926, sumOfPerimeter(shp), EPSLION);
     //FAIL("123456");
 }
 
