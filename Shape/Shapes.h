118485b51d7bd6450aa4b46c3e3ece8ea46351cb
MingE <coopldh@gmail.com>
2016-09-23 22:09:29 +0800

    Complete computing  the perimeter of a triangle

--- Shape/Shapes.h
+++ Shape/Shapes.h
@@ -1,6 +1,8 @@
 #ifndef SHAPES_H_INCLUDED
 #define SHAPES_H_INCLUDED
 
+#include <cmath>
+
 double PI=3.1415926;
 
 class Shape{
@@ -12,7 +14,7 @@ public:
 class Circle : public Shape{
 public:
     Circle(double centerX, double centerY, double radius):
-        cx(centerX), cy(centerY), r(radius){}
+        cx(centerX),cy(centerY),r(radius){}
     double area() const {return PI*r*r;}
     double perimeter() const {return PI*2*r;}
 private:
@@ -22,11 +24,27 @@ private:
 class Rectangle : public Shape{
 public:
     Rectangle(double leftTopX, double leftTopY, double length, double width):
-        x(leftTopX), y(leftTopY), l(length), w(width){}
+        x(leftTopX),y(leftTopY),l(length),w(width){}
     double area() const {return l*w;}
     double perimeter() const {return 2*(l+w);}
 private:
     double x,y,l,w;
+};
+
+class Triangle : public Shape{
+public:
+    Triangle(double x1, double y1, double x2, double y2, double x3, double y3):
+        ax(x1),ay(y1),bx(x2),by(y2),cx(x3),cy(y3){}
+    double area() const {return 0;}
+    double perimeter() const {
+        double abLength=sqrt(pow(ax-bx,2) + pow(ay-by,2));
+        double acLength=sqrt(pow(ax-cx,2) + pow(ay-cy,2));
+        double bcLength=sqrt(pow(bx-cx,2) + pow(by-cy,2));
+
+        return abLength + acLength + bcLength;
+    }
+private:
+    double ax,ay,bx,by,cx,cy;
 };
 
 #endif // SHAPES_H_INCLUDED
--- Shape/UnitShapes.h
+++ Shape/UnitShapes.h
@@ -6,16 +6,23 @@
 
 const double EPSLION = 0.000001;
 TEST (first, Circle) {
-    Circle cir(0,0,10);
+    Circle cir(0,0, 10);
     DOUBLES_EQUAL(314.15926,cir.area(),EPSLION);
     DOUBLES_EQUAL(62.831852,cir.perimeter(),EPSLION);
     //FAIL("123456");
 }
 
 TEST (second, Rectangle) {
-    Rectangle rect(0,0,2,5);
+    Rectangle rect(0,0, 2,5);
     DOUBLES_EQUAL(10,rect.area(),EPSLION);
     DOUBLES_EQUAL(14,rect.perimeter(),EPSLION);
+    //FAIL("123456");
+}
+
+TEST (thrid, Triangle) {
+    Triangle tri(1,-1, 4,-1, 4,3);
+    //DOUBLES_EQUAL(6,tri.area(),EPSLION);
+    DOUBLES_EQUAL(12,tri.perimeter(),EPSLION);
     //FAIL("123456");
 }
 
