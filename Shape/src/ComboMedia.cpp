8839456009efb8ae1e87c0d99bfc25ce5bd32d86
MingE <coopldh@gmail.com>
2016-10-19 14:30:09 +0800

    Fixed HW3-2, 3-3 形狀合成方法

--- Shape/src/ComboMedia.cpp
+++ Shape/src/ComboMedia.cpp
@@ -5,9 +5,9 @@ vector<Media*> ComboMedia::getMediaPieces() const{return _mediaPieces;}
 void ComboMedia::add(Media* mediaPieces){_mediaPieces.push_back(mediaPieces);}
 
 void ComboMedia::accept(MediaVisitor* mediaVisitor){
-    mediaVisitor->visitComboMedia(this);
     for(Media *m: _mediaPieces)
         m->accept(mediaVisitor);
+    mediaVisitor->visitComboMedia(this);
 }
 
 
