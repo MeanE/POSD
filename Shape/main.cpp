c010e23820e430d470adb9bbf52a0ae2473c5065
MingE <coopldh@gmail.com>
2016-12-30 22:43:51 +0800

    Set Test Mode & Console Mode

--- Shape/main.cpp
+++ Shape/main.cpp
@@ -1,15 +1,8 @@
-#include "..\cppunitlite\TestHarness.h"
-#include "UnitShapes.h"
-#include "UnitMedia.h"
 #include "TextUI.h"
 using namespace std;
 
 int main()
 {
-    TestResult tr;
-    TestRegistry::runAllTests(tr);
-    cout<<endl;
-
     TextUI textUI;
     textUI.run();
     return 0;
---
+++ Shape/test_main.cpp
@@ -0,0 +1,10 @@
+#include "..\cppunitlite\TestHarness.h"
+#include "UnitShapes.h"
+#include "UnitMedia.h"
+using namespace std;
+
+int main()
+{
+    TestResult tr;
+    TestRegistry::runAllTests(tr);
+}
