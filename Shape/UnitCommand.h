3d3173269821a71326c8d51658998ce885f214ac
MingE <coopldh@gmail.com>
2016-12-31 18:15:24 +0800

    Complete Execute in DefineCommand

--- Shape/UnitCommand.h
+++ Shape/UnitCommand.h
@@ -8,12 +8,47 @@
 #include "Builders.h"
 #include "Commands.h"
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 const double EPSLION_IN_UNITCOMMAND=0.000001;
 
-TEST (Test, Test) {
-    FAIL("test");
+TEST (Execute_Define, DefineCommand) {
+    /**def cSmall = Circle(2,1,1)*/
+    /**def rTall = Rectangle(1,10,2,8)*/
+    /**def tBig = Triangle(1,22,34,-4,15,16)*/
+    /**def comboExclamation = combo{rTall,cSmall}*/
+
+    string content[] = {
+        "cSmall = Circle(2,1,1)",
+        "rTall = Rectangle(1,10,2,8)",
+        "tBig = Triangle(1,-1, 4,-1, 4,3)",
+        "comboExclamation = combo{rTall,cSmall,tBig}"
+    };
+    map<string, Media*> medias;
+    for(int i=0; i<4; i++){
+        DefineCommand defineCommand(content[i], &medias);
+        defineCommand.Execute();
+    }
+    /**clear console*/
+    system("cls");
+
+    /**STL map default use ascending sort by key*/
+    string description[]={
+        "c(2 1 1) ",
+        "combo(r(1 10 2 8) c(2 1 1) t(1 -1 4 -1 4 3) )",
+        "r(1 10 2 8) ",
+        "t(1 -1 4 -1 4 3) "
+    };
+
+    int i=0;
+    for(auto it: medias){
+        Media* m = it.second;
+        DescriptionVisitor dv;
+        m->accept(&dv);
+        //cout<<dv.getDescription()<<endl;
+        CHECK(description[i++] == dv.getDescription());
+    }
 }
 
 #endif // UNITCOMMAND_H_INCLUDED
--- Shape/include/DefineCommand.h
+++ Shape/include/DefineCommand.h
@@ -2,19 +2,23 @@
 #define DEFINECOMMAND_H
 
 #include "Command.h"
-
+#include "Media.h"
+#include <string>
+#include <map>
+using namespace std;
 
 class DefineCommand : public Command
 {
 public:
-    DefineCommand();
-    ~DefineCommand();
+    DefineCommand(string content, map<string, Media*>* medias);
+    ~DefineCommand(){}
     void Execute();
     void Undo();
     void Redo();
 
 private:
-
+    string _content;
+    map<string, Media*>* _medias;
 };
 
 #endif // DEFINECOMMAND_H
